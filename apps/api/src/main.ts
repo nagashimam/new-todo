4a78772996058aedc494080c3e2c82df72549c2d
masato <masato.ngsm@gmail.com>
2022-02-23 20:52:30 +0900

    CORSの設定を追加

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const cors = {
+    origin: 'http://localhost:4200',
+    method: 'GET,POST,DELETE,PATCH',
+  };
+  const app = await NestFactory.create(AppModule, { cors });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;

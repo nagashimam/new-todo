1fa95aefcb221aacbc3c57dac138f0b8b9932cdd
masato <masato.ngsm@gmail.com>
2022-02-26 16:01:28 +0900

    ログイン画面とアカウント登録画面を追加

--- apps/todo/src/app/app-routing.module.ts
+++ apps/todo/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
   { path: 'todos', component: TodosComponent },
-  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- apps/todo/src/app/app.module.ts
+++ apps/todo/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent],
+  declarations: [AppComponent, TodosComponent, LoginComponent, RegistrationComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],

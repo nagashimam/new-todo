46679b8bd8d776bf54ea7294fb8b3fafaa330bbd
masato <masato.ngsm@gmail.com>
2022-02-26 17:19:08 +0900

    パスワードをハッシュ化してからDBに保存する

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { HashService } from '../hash/hash.service';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private hashService: HashService
+  ) {}
 
   @Post()
   create(@Body() data: Prisma.UserCreateInput) {
+    data.password = this.hashService.hash(data.password);
     return this.userService.create(data);
   }
 
--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { HashModule } from '../hash/hash.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HashModule],
   controllers: [UserController],
   providers: [UserService],
 })

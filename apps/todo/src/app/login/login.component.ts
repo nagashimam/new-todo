ff4c8d9a79e0994cbc1cb83ffa04fd0485e49daf
masato <masato.ngsm@gmail.com>
2022-03-03 00:24:01 +0900

    認証済みであれば自動でログインする

--- apps/todo/src/app/login/login.component.ts
+++ apps/todo/src/app/login/login.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
-import { skip } from 'rxjs';
-import { UserQuery } from '../service/user/user.query';
-import { UserService } from '../service/user/user.service';
 
 @Component({
   selector: 'todo-login',
@@ -12,67 +8,15 @@ import { UserService } from '../service/user/user.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  error: string | undefined = undefined;
-  label: { title: string; button: string; linkText: string; linkHref: string };
-
-  constructor(
-    private userQuery: UserQuery,
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private router: Router,
-    public auth: AuthService
-  ) {
-    this.label = this.router.url.includes('login')
-      ? {
-          title: 'Login',
-          button: 'ログイン',
-          linkText: 'アカウント登録',
-          linkHref: './registration',
-        }
-      : {
-          title: 'Registration',
-          button: '登録',
-          linkText: 'ログイン',
-          linkHref: './login',
-        };
-
-    this.userQuery.user$.pipe(skip(1)).subscribe((user) => {
-      if (user.id && user.password) {
-        this.router.navigate(['/todos']);
+  constructor(public auth: AuthService, private router: Router) {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('./todos');
       }
     });
-    this.userQuery.selectError().subscribe((error) => {
-      this.error = error.error.message;
-      this.loginFormGroup.reset();
-    });
   }
-  loginFormGroup = this.formBuilder.group({
-    id: ['', Validators.required],
-    password: [
-      '',
-      Validators.compose([
-        Validators.required,
-        Validators.maxLength(20),
-        Validators.minLength(8),
-      ]),
-    ],
-  });
 
   loginWithRedirect() {
     this.auth.loginWithRedirect();
-  }
-
-  logoutWithRedirect() {
-    this.auth.logout();
-  }
-
-  submit() {
-    const id = this.loginFormGroup.controls['id'].value;
-    const password = this.loginFormGroup.controls['password'].value;
-    if (this.label.title === 'Login') {
-      this.userService.findOne({ id, password });
-    } else {
-      this.userService.create({ id, password });
-    }
   }
 }
